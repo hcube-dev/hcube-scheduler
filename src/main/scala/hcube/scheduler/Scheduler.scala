abdef70ef91dc089a85961a19173d96e03ccf17d
Michal <michal@mdevel.net>
2017-02-16 13:00:48 +0100

    initial scheduler loop

--- src/main/scala/hcube/scheduler/Scheduler.scala
+++ src/main/scala/hcube/scheduler/Scheduler.scala
@@ -3,8 +3,7 @@ package hcube.scheduler
 import hcube.scheduler.backend.Backend
 import hcube.scheduler.utils.TimeUtil
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.annotation.tailrec
 
 trait Scheduler {
 
@@ -14,25 +13,34 @@ trait Scheduler {
 
 class LoopScheduler(
   backend: Backend,
-  interval: Long
+  delta: Long = 1000,
+  tolerance: Long = 50
 ) extends Scheduler {
 
   override def apply(): Unit = {
     val now = System.currentTimeMillis()
-    val intervalStart = (now / interval) * interval
-    if (now - intervalStart > 0) {
-      TimeUtil.sleep((intervalStart + interval) - now)
+    val t0 = (now / delta) * delta
+    loop(t0)
+  }
+
+  /**
+    * [t0, t1] - time boundaries for current interval
+    * t0 - beginning time of the current interval
+    * t1 - end time of the current interval
+    * @param t0
+    */
+  @tailrec private def loop(t0: Long): Unit = {
+    val now = System.currentTimeMillis()
+    val t1 = t0 + delta
+
+    if (now - t0 > tolerance) {
+      // sleep till next interval
+      TimeUtil.sleep(t1 - now)
     }
 
-    while(true) {
-      val now = System.currentTimeMillis()
-      val start = now / interval
+    // TODO process jobs
 
-      val jobs = Await.result(backend.pullJobs(), Duration.Inf)
-
-      val t1 = System.currentTimeMillis()
-    }
-
+    loop(t1)
   }
 
 }
