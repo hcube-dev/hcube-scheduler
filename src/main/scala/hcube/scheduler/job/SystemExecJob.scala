94ccd348b7e963960b764ea95538d878c376b9e7
Michal <michal@mdevel.net>
2017-02-17 14:01:59 +0100

    added logging to system exec job

--- src/main/scala/hcube/scheduler/job/SystemExecJob.scala
+++ src/main/scala/hcube/scheduler/job/SystemExecJob.scala
@@ -1,6 +1,9 @@
 package hcube.scheduler.job
 
+import java.time.{Instant, ZonedDateTime}
+
 import com.typesafe.scalalogging.Logger
+import hcube.scheduler.utils.TimeUtil
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.sys.process.Process
@@ -10,6 +13,7 @@ class SystemExecJob(implicit ec: ExecutionContext) extends Job {
   val logger = Logger(getClass)
 
   override def apply(time: Long, payload: Map[String, Any]): Unit = {
+    logger.debug(s"Executing SystemExecJob, time: ${ZonedDateTime.ofInstant(Instant.ofEpochMilli(time), TimeUtil.UTC)}, payload: $payload")
     payload.get("cmd").foreach { case cmd: String =>
       Future {
         val code = Process(cmd).!
