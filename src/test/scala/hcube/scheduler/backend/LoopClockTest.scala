f6e430af432776ff668b3ffec12ce9ab23facc1b
Michal <michal@mdevel.net>
2017-02-18 15:42:36 +0100

    improved loop clock test

--- src/test/scala/hcube/scheduler/backend/LoopClockTest.scala
+++ src/test/scala/hcube/scheduler/backend/LoopClockTest.scala
@@ -1,5 +1,6 @@
 package hcube.scheduler.backend
 
+import hcube.scheduler.utils.TimeUtil.SleepFn
 import hcube.scheduler.{JobExecutor, LoopClock}
 import org.mockito.{ArgumentCaptor, Mockito => MockitoMockito}
 import org.specs2.mock.Mockito
@@ -21,6 +22,8 @@ class LoopClockTest extends Specification with Mockito {
     timeQueue.dequeue()
   }
 
+  private val sleep = mock[SleepFn]
+
   "scheduler" >> {
     val clock = new LoopClock(
       executor,
@@ -28,7 +31,7 @@ class LoopClockTest extends Specification with Mockito {
       tolerance = 50,
       continueOnInterrupt = false,
       currentTimeMillis = timeFn,
-      sleep = (_: Long) => (),
+      sleep = sleep,
       stopTime = Some(7000)
     )
 
@@ -39,8 +42,14 @@ class LoopClockTest extends Specification with Mockito {
     MockitoMockito.verify(executor, MockitoMockito.times(6))
       .tick(t0Arg.capture(), t1Arg.capture())
 
+    val sleepArg = ArgumentCaptor.forClass(classOf[Long])
+    MockitoMockito.verify(sleep, MockitoMockito.times(5))
+      .apply(sleepArg.capture())
+
     List(1000L, 2000L, 3000L, 4000L, 5000L, 7000L) must_== t0Arg.getAllValues.toList
     List(2000L, 3000L, 4000L, 5000L, 6000L, 8000L) must_== t1Arg.getAllValues.toList
+
+    List(960L, 1500L, 980L, 800L, 800L) must_== sleepArg.getAllValues.toList
   }
 
 }
